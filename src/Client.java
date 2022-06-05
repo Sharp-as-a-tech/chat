dcb5edbe51adc495c2724aba717cb599cd84f301
zahidasafi <safi.zahida2019@gmail.com>
2022-06-05 07:56:35 +0430

    some libraries were added

--- src/Client.java
+++ src/Client.java
@@ -1,3 +1,8 @@
+
+import java.io.BufferedReader;
+import java.io.InputStreamReader;
+import java.io.PrintWriter;
+import java.net.Socket;
 public class Client {
     Socket socket;
     BufferedReader br;
