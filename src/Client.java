acd339f8c8ed7a3a26982c1c73e6da83f315f30f
SoHailaNekHat <sohaila.wb@gmail.com>
2022-06-05 07:34:20 +0430

    first commit

--- src/Client.java
+++ src/Client.java
@@ -1,2 +1,23 @@
 public class Client {
+    Socket socket;
+    BufferedReader br;
+    PrintWriter out;
+
+    //class constructor
+    public Client() {
+        try {
+            System.out.println("Sending request to Server");
+            socket = new Socket("127.0.0.1", 7777);
+            System.out.println("Connection done");
+            br = new BufferedReader(new InputStreamReader(socket.getInputStream()));
+            out = new PrintWriter(socket.getOutputStream());
+            startReading();
+            startWriting();
+
+        } catch (Exception e) {
+
+        }
+    }
 }
+
+
--- src/Server.java
+++ src/Server.java
@@ -1,2 +1,28 @@
 public class Server {
+    ServerSocket server;
+    Socket socket;
+    BufferedReader br;
+    PrintWriter out;
+    //constructor
+    public Server(){
+
+        try{
+            server=new ServerSocket(7777);
+            System.out.println("server is ready to accept connection");
+            System.out.println("waiting...");
+            socket=server.accept();
+            br=new BufferedReader(new InputStreamReader(socket.getInputStream()));
+            out=new PrintWriter(socket.getOutputStream());
+            startReading();
+            startWriting();
+        }
+        catch (Exception e){
+            e.printStackTrace();
+        }
+    }
+
+
+
+
+
 }
